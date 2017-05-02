9b80192ba22eecf808e9adc7ca02625788e0bb47
Ari Russo <ari.russo@gmail.com>
2017-05-02 17:13:24 -0400

    yarddoc

--- lib/viddl/video/clip.rb
+++ lib/viddl/video/clip.rb
@@ -4,24 +4,52 @@ module Viddl
 
     class Clip
 
+      # Create a clip using the given video source file path and options
+      # @param [String] source_path Path to video file to create clip from
+      # @param [Hash] options
+      # @option options [Numeric] :start Time in the source file where the clip starts
+      # @option options [Numeric] :duration Duration of the clip
+      # @option options [Numeric] :end Time in the source file where the clip ends
+      # @return [Clip]
       def self.process(path, options = {})
-        new(path).process(options)
+        clip = new(path)
+        clip.process(options)
+        clip
       end
 
-      def initialize(path)
-        @source_path = path
+      # @param [String] source_path Path to video file to create clip from
+      def initialize(source_path)
+        @source_path = source_path
       end
 
+      # Create a clip using the given options
+      # @param [Hash] options
+      # @option options [Numeric] :start Time in the source file where the clip starts
+      # @option options [Numeric] :duration Duration of the clip
+      # @option options [Numeric] :end Time in the source file where the clip ends
+      # @return [Boolean]
       def process(options = {})
         system(command_line(options))
       end
 
       private
 
-      def command_line(options)
+      # Command line to create a clip from the source file and given options
+      # @param [Hash] options
+      # @option options [Numeric] :start Time in the source file where the clip starts
+      # @option options [Numeric] :duration Duration of the clip
+      # @option options [Numeric] :end Time in the source file where the clip ends
+      # @return [String]
+      def command_line(options = {})
         "ffmpeg -i #{@source_path} #{time_args(options)} -c:v copy -c:a copy #{output_path}"
       end
 
+      # Command line options for the given time constraints
+      # @param [Hash] options
+      # @option options [Numeric] :start Time in the source file where the clip starts
+      # @option options [Numeric] :duration Duration of the clip
+      # @option options [Numeric] :end Time in the source file where the clip ends
+      # @return [String]
       def time_args(options = {})
         args = ""
         if !options[:start].nil?
@@ -38,6 +66,8 @@ module Viddl
         args
       end
 
+      # Path of the created clip
+      # @return [String]
       def output_path
         @source_path.scan(/#{Download::TEMPDIR}\/(.*)/).flatten.first
       end
