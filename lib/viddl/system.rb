b4d8c5a8f530b35140f784c798d83f54279d7950
Ari Russo <ari.russo@gmail.com>
2017-05-02 17:21:17 -0400

    yard

--- lib/viddl/system.rb
+++ lib/viddl/system.rb
@@ -4,20 +4,28 @@ module Viddl
 
     extend self
 
+    # Validate that the system has all of its dependencies
+    # @return [Boolean]
     def validate
       validate_youtube_dl
       validate_ffmpeg
+      true
     end
 
     private
 
+    # Validate that ffmpeg is installed
+    # @return [Boolean]
     def validate_ffmpeg
       result = system("ffmpeg")
       if result.nil?
         raise("Viddl requires that ffmpeg be installed")
       end
+      true
     end
 
+    # Validate that youtube-dl is installed
+    # @return [Boolean]
     def validate_youtube_dl
       result = system("youtube-dl")
       if result.nil?
--- lib/viddl/video.rb
+++ lib/viddl/video.rb
@@ -8,6 +8,10 @@ module Viddl
 
     extend self
 
+    # Download a video using the given url
+    # @param [String] url
+    # @param [Hash] options
+    # @return [Video::Instance]
     def download(url, options = {})
       video = Instance.new(url)
       video.download(options)
