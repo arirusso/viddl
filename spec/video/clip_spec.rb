90015034b88303f86445bbaecf23343acb7e04af
Ari Russo <ari.russo@gmail.com>
2017-05-02 17:07:07 -0400

    more clip tests

--- spec/video/clip_spec.rb
+++ spec/video/clip_spec.rb
@@ -25,6 +25,24 @@ describe Viddl::Video::Clip do
       expect(args).to(eq(" -ss 10 -t 5"))
     end
 
+    it "has correct args with start time and duration" do
+      options = {
+        start: 10,
+        duration: 15
+      }
+      args = @clip.send(:time_args, options)
+      expect(args).to(eq(" -ss 10 -t 15"))
+    end
+
+    it "raises if end time and duration are included" do
+      options = {
+        start: 10,
+        end: 15,
+        duration: 3
+      }
+      expect { @clip.send(:time_args, options) }.to(raise_error(RuntimeError))
+    end
+
   end
 
 end
