c6ac1bcc53a35a41cd2f1364e82db93377a0e85a
Ari Russo <ari.russo@gmail.com>
2017-05-03 19:42:59 -0400

    test for Download#command_line

--- spec/video/download_spec.rb
+++ spec/video/download_spec.rb
@@ -3,7 +3,8 @@ require "helper"
 describe Viddl::Video::Download do
 
   before(:each) do
-    @source_url = "https://youtube.com/watch?v=6g4dkBF5anU.mkv"
+    @video_id = "6g4dkBF5anU"
+    @source_url = "https://youtube.com/watch?v=#{@video_id}"
     @video = Viddl::Video::Instance.new(@source_url)
     @download = Viddl::Video::Download.new(@video)
   end
@@ -18,6 +19,22 @@ describe Viddl::Video::Download do
         @result = @download.send(:process, @options)
       end
 
+    end
+
+  end
+
+  context "#command_line" do
+
+    before(:each) do
+      @result = @download.send(:command_line)
+    end
+
+    it "includes url" do
+      expect(@result).to(include(@source_url))
+    end
+
+    it "includes temp file path" do
+      expect(@result).to(include("#{Viddl::Video::Download::TEMPDIR}/#{@video_id}"))
     end
 
   end
