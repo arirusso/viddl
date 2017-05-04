b9969ef1251878dbda1f7958a3a94a1881cb3c58
Ari Russo <ari.russo@gmail.com>
2017-05-03 21:06:42 -0400

    tests for Instance#clip

--- spec/video/instance_spec.rb
+++ spec/video/instance_spec.rb
@@ -8,6 +8,78 @@ describe Viddl::Video::Instance do
     @video = Viddl::Video::Instance.new(@source_url)
   end
 
+  context "#clip" do
+
+    before(:each) do
+      @video.instance_variable_set("@download", Object.new)
+      @video.stub(:source_filenames).and_return(["/tmp/#{@video_id}.mkv"])
+    end
+
+    context "with no options" do
+
+      it "execs command line" do
+        @options = {}
+        expect(Kernel).to(receive(:system))
+        @result = @video.clip(@options)
+      end
+
+    end
+
+    context "with duration" do
+
+      it "execs command line" do
+        @options = {
+          duration: 12
+        }
+        expect(Kernel).to(receive(:system))
+        @result = @video.clip(@options)
+      end
+
+    end
+
+    context "with start time and duration" do
+
+      it "execs command line" do
+        @options = {
+          start: 10,
+          duration: 15
+        }
+        expect(Kernel).to(receive(:system))
+        @result = @video.clip(@options)
+      end
+
+    end
+
+    context "with start and end time" do
+
+      it "execs command line" do
+        @options = {
+          start: 8,
+          end: 15
+        }
+        expect(Kernel).to(receive(:system))
+        @result = @video.clip(@options)
+      end
+
+    end
+
+    context "with start, end time and duration" do
+
+      it "raises" do
+        @options = {
+          duration: 5,
+          end: 15,
+          start: 10
+        }
+        expect {
+          @video.clip(@options)
+        }.to(raise_error(RuntimeError))
+      end
+
+    end
+
+  end
+
   context "#download" do
 
     context "with no options" do
