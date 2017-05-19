3f22bf8c115e987453aec0164163178a34a1a11b
Ari Russo <ari.russo@gmail.com>
2017-05-18 22:13:07 -0400

    add option to pass flags to youtube-dl

--- lib/viddl/video/download.rb
+++ lib/viddl/video/download.rb
@@ -14,6 +14,7 @@ module Viddl
       # Download the given video
       # @param [Video::Instance] video
       # @param [Hash] options
+      # @option options [String] :flags Flags to pass to youtube-dl
       # @return [Download]
       def self.process(video, options = {})
         download = new(video)
@@ -29,9 +30,11 @@ module Viddl
 
       # Download the video file
       # @param [Hash] options
+      # @option options [String] :flags Flags to pass to youtube-dl
       # @return [Boolean]
       def process(options = {})
-        result = Kernel.system(command_line)
+        cmd = command_line(options)
+        result = Kernel.system(cmd)
         raise(result.to_s) unless result
         true
       end
@@ -39,9 +42,11 @@ module Viddl
       private
 
       # Command line to download the video file
+      # @param [Hash] options
+      # @option options [String] :flags Flags to pass to youtube-dl
       # @return [String]
-      def command_line
-        "youtube-dl #{@video.source_url} #{FORMAT_ARG} -o '#{TEMPDIR}/#{@video.id}s.%(ext)s'"
+      def command_line(options = {})
+        "youtube-dl #{@video.source_url} #{FORMAT_ARG} #{options[:flags]} -o '#{TEMPDIR}/#{@video.id}s.%(ext)s'"
       end
 
     end
